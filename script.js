172f747111dc47120da2521605b4332814e8edb0
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-07-14 12:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //your JS code here. If required.
 
-let result = text.toUpperCase();
-document.getElementById("fname").innerHTML = result;
+ function toUpperCaseOnBlur() {
+            var inputField = document.getElementById('fname');
+            inputField.addEventListener('blur', function() {
+                inputField.value = inputField.value.toUpperCase();
+            });
+        }
+
+        // Call the function when the DOM is fully loaded
+        window.onload = function() {
+            toUpperCaseOnBlur();
+        }
